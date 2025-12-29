fb2c685d90c0202c19d69f967d9439f002d5d4a3
luxevistainfo-dev <luxevistainfo@gmail.com>
2025-12-29 23:31:41 +0100

    Create keys.ts

---
+++ src/wallet/keys.ts
@@ -0,0 +1,10 @@
+import crypto from "crypto";
+
+export function generateKeyPair(): { publicKey: string; privateKey: string } {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: { type: "spki", format: "pem" },
+    privateKeyEncoding: { type: "pkcs8", format: "pem" },
+  });
+  return { publicKey, privateKey };
+}
