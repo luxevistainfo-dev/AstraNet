7195326d952c38d3b0052dae3ced3b387be3fc82
luxevistainfo-dev <luxevistainfo@gmail.com>
2025-12-29 23:32:42 +0100

    Create index.ts

---
+++ src/explorer/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { Blockchain } from "../core/blockchain";
+import { setupRoutes } from "./routes";
+
+const app = express();
+const PORT = 4000;
+
+const blockchain = new Blockchain();
+setupRoutes(app, blockchain);
+
+app.listen(PORT, () => {
+  console.log(`Explorer running at http://localhost:${PORT}`);
+});
